726dc7b7610bdd27889beb9e521ab87585c2399c
pedroyan9 <santosyan22222@gmail.com>
2024-03-22 19:50:54 -0300

    Organização do Código

--- dados.py
+++ dados.py
@@ -1,18 +1,14 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import plotly.express as px
-
-# Alinhe o título no centro e defina a cor vermelha
-st.markdown("<h1 style='text-align: center; color: red;'>Titulos Copa Do Brasil</h1>", unsafe_allow_html=True)
-
-#st.sidebar.title('Titulo Copa Do Brasil')
-#st.sidebar.write('ano')
 
 # Carregue os dados do CSV (substitua pelo seu caminho)
 df = pd.read_csv('hall.csv')
 
-# Variável para controlar a exibição do gráfico
+# Alinhe o título no centro e defina a cor vermelha
+st.markdown("<h1 style='text-align: center; color: red;'>Títulos Copa do Brasil</h1>", unsafe_allow_html=True)
+
+# Variáveis para controlar a exibição do gráfico
 show_campeao = st.button("Campeão")
 show_vice = st.button("Vice")
 
@@ -21,13 +17,6 @@ if show_campeao:
     st.dataframe(df['Campeão'])
 elif show_vice:
     st.dataframe(df['Vice'])
-
-# Carregue os dados do CSV (substitua pelo seu caminho)
-df = pd.read_csv('hall.csv')
-
-# Create a variable for your "Vice" data
-vice_data = df['Vice']
-
 
 # Crie um gráfico de barras com pontos
 fig = px.bar(df, x='Campeão')
@@ -38,23 +27,10 @@ fig.update_traces(marker=dict(size=10, color='red'), selector=dict(mode='markers
 # Crie um layout para o dashboard
 fig.update_layout(
     xaxis_title='Campeão',
-    yaxis_title='titulos',
+    yaxis_title='Títulos',
     font=dict(size=14),
     showlegend=False
 )
 
 # Mostre o dashboard
 st.plotly_chart(fig)
-
-# Adicione uma cor de fundo à página
-st.markdown(
-    """
-    <style>
-        body {
-            background-color: #00008B; /* Substitua pela cor desejada */
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
